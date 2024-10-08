7a2ac6d43799c6b666af745bec8baf6c7d59c0d9
Samson Mubeya <rasspartah@gmail.com>
2024-10-08 23:26:57 +0300

    added bottom tabs for navigation

---
+++ components/Grow.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Grow = () => {
+  return (
+    <View>
+      <Text>Grow</Text>
+    </View>
+  )
+}
+
+export default Grow
